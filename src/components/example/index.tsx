d645b9499fa5a6a570354ade03c6534a968c891b
alireza_amini <aminicss1@gmail.com>
2022-08-08 14:54:54 +0430

    chore: move lint staged to package.json

--- src/components/example/index.tsx
+++ src/components/example/index.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 
 export interface ExampleProps {
-  title: string;
-  description: string;
+	title: string;
+	description: string;
 }
 
-const Example: React.FunctionComponent<ExampleProps> = (Props) => {
-  console.log('console log');
-  return (
-    <div>
-      <p>title: {Props.title}</p>
-      Example
-      <p>description: {Props.description}</p>
-    </div>
-  );
+const Example: React.FunctionComponent<ExampleProps> = Props => {
+	return (
+		<div>
+			<p>title: {Props.title}</p>
+			Example
+			<p>description: {Props.description}</p>
+		</div>
+	);
 };
 
 export default Example;
