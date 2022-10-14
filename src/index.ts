f995a6d32a5c6bd3dbd335724ee2e8e7a7439e33
alireza_amini <aminicss1@gmail.com>
2022-10-14 22:26:13 +0330

    fix: export types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import Button from './components/button';
 import * as BillboardTypes from './types';
+import Button from './components/button';
+import { GlobalStyle } from './styles/_global';
 
-export { GlobalStyle } from './styles/_global';
-export { BillboardTypes, Button };
+export { BillboardTypes, Button, GlobalStyle };
