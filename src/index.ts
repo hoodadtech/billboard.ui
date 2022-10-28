2d855daa4f072d163c25362ba152f26edb20ba0a
alireza_amini <aminicss1@gmail.com>
2022-10-28 14:52:39 +0330

    feat: upgrade version

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as BillboardTypes from './types';
 import Button from './components/button';
+import { ThemeContext, ThemeProvider } from './components/theme-provider';
 import { GlobalStyle } from './styles/_global';
 
-export { BillboardTypes, Button, GlobalStyle };
+export { BillboardTypes, Button, GlobalStyle, ThemeProvider, ThemeContext };
